94d8d5d88f8005217fc91e8c32c3e1d99c20cff5
mr_ceh <webmaster.webdeveloper@gmail.com>
2021-11-23 04:58:25 -0800

    add account list ui

--- src/components/networks/AccountList.vue
+++ src/components/networks/AccountList.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="col-12">
-        {{value}}
+        <div class="col-12 box" v-for="(account , index) in accountList" :key="index">
+            <div class="acc-box" v-if="account.name">
+                <i class="mdi mdi-account"></i> {{account.name}}({{account.authority}})
+            </div>
+        </div>
     </div>
 </template>
 <script lang="ts">
@@ -10,7 +14,25 @@ import {Vue, Component , Prop , Watch} from 'vue-property-decorator'
 })
 export default class AccountList extends Vue{
     @Prop({default:() =>{return []}}) value:any;
-
+    accountList:any=[];
+    mounted(){
+        this.accountList = this.value;
+    }
 
 }
 </script>
+<style lang="scss" scoped>
+.box{
+    background: #f8f8fb;
+    width: 100%;
+    box-shadow: 0 0.75rem 1.5rem rgb(18 38 63 / 3%);
+    .acc-box{
+        box-shadow: 0 0.75rem 1.5rem rgb(18 38 63 / 3%);
+        background: #ffffff;
+        padding: 10px ;
+        margin: 15px;
+
+    }
+
+}
+</style>
