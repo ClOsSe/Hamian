f093366b396d0e9207d22897bfa4e566ddb4fc90
mr_ceh <webmaster.webdeveloper@gmail.com>
2021-11-23 09:47:18 -0800

    fix shadow

--- src/components/networks/Resources.vue
+++ src/components/networks/Resources.vue
@@ -1,7 +1,48 @@
 <template>
-    <div class="col-12">
-        {{value}}
-
+    <div class="col-12 text-center py-2">
+        <h5 class="font-size-15 mb-4">Resources</h5>
+        <div class="col-12 d-flex">
+          <div class="col box" >
+              <div class="text-center">
+                  <apexchart 
+                  class="apex-charts"
+                  type="radialBar"
+                  height="150"
+                  dir="ltr"
+                  :series="RAMSeries"
+                  :options="RAMChartOptions"
+                  ></apexchart>
+                  <p class="text-muted ">Total:{{}}</p>
+              </div>
+          </div>
+          <div class="col box" >
+              <div class="text-center">
+                  <apexchart 
+                  class="apex-charts"
+                  type="radialBar"
+                  height="150"
+                  dir="ltr"
+                  :series="CPUSeries"
+                  :options="CPUChartOptions"
+                  ></apexchart>
+                  <p class="text-muted ">Total:{{}}</p>
+              </div>
+          </div>
+          <div class="col box" >
+              <div class="text-center">
+                  <apexchart 
+                  class="apex-charts"
+                  type="radialBar"
+                  height="150"
+                  dir="ltr"
+                  :series="NETSeries"
+                  :options="NETChartOptions"
+                  ></apexchart>
+                  <p class="text-muted ">Total:{{}}</p>
+              </div>
+        
+          </div>
+        </div>
     </div>
 </template>
 <script lang="ts">
@@ -11,7 +52,152 @@ import {Vue, Component , Prop , Watch} from 'vue-property-decorator'
 })
 export default class AccountList extends Vue{
     @Prop({default:() =>{return []}}) value:any;
+    RAMSeries:any= [80];
+    CPUSeries:any= [62];
+    NETSeries:any= [36];
+    mounted(){
+      console.log('this is value ',this.value)
+    }
+    RAMChartOptions:any= {
+        chart: {
+          height: 150,
+          type: "radialBar",
+          sparkline: {
+            enabled: true,
+          },
+        },
+        colors: ["#0a869f"],
+        plotOptions: {
+          radialBar: {
+            startAngle: 0,
+            endAngle: 360,
+            track: {
+              background: "#e7e7e7",
+              strokeWidth: "90%",
+              margin: 5, // margin is in pixels
+            },
+            hollow: {
+              size: "60%",
+            },
+
+            dataLabels: {
+              name: {
+                show: true,
+              },
+              value: {
+                offsetY: 5,
+                fontSize: "16px",
+              },
+            },
+          },
+        },
+        grid: {
+          padding: {
+            top: 0,
+          },
+        },
+        stroke: {
+          dashArray: 3,
+        },
+        labels: ["RAM"],
+      }
+    CPUChartOptions:any= {
+        chart: {
+          height: 150,
+          type: "radialBar",
+          sparkline: {
+            enabled: true,
+          },
+        },
+        colors: ["#001dc3"],
+        plotOptions: {
+          radialBar: {
+            startAngle: 0,
+            endAngle: 360,
+            track: {
+              background: "#e7e7e7",
+              strokeWidth: "90%",
+              margin: 5, // margin is in pixels
+            },
+            hollow: {
+              size: "60%",
+            },
+
+            dataLabels: {
+              name: {
+                show: true,
+              },
+              value: {
+                offsetY: 5,
+                fontSize: "16px",
+              },
+            },
+          },
+        },
+        grid: {
+          padding: {
+            top: 0,
+          },
+        },
+        stroke: {
+          dashArray: 3,
+        },
+        labels: ["CPU"],
+      }
+    NETChartOptions:any= {
+        chart: {
+          height: 150,
+          type: "radialBar",
+          sparkline: {
+            enabled: true,
+          },
+        },
+        colors: ["#046011"],
+        plotOptions: {
+          radialBar: {
+            startAngle: 0,
+            endAngle: 360,
+            track: {
+              background: "#e7e7e7",
+              strokeWidth: "90%",
+              margin: 5, // margin is in pixels
+            },
+            hollow: {
+              size: "60%",
+            },
+
+            dataLabels: {
+              name: {
+                show: true,
+              },
+              value: {
+                offsetY: 5,
+                fontSize: "16px",
+              },
+            },
+          },
+        },
+        grid: {
+          padding: {
+            top: 0,
+          },
+        },
+        stroke: {
+          dashArray: 3,
+        },
+        labels: ["NET"],
+      }
+
 
 
 }
 </script>
+<style lang="scss">
+.box{
+  background: #ffffff;
+  margin:5px;
+  margin-top:0px;
+  box-shadow: 0 0.75rem 1.5rem rgb(18 38 63 / 10%);
+
+}
+</style>
