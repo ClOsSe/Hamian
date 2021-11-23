50b0bb8eb644ca570c7b7eaa17ed9a8b29eeb856
mr_ceh <webmaster.webdeveloper@gmail.com>
2021-11-23 09:47:42 -0800

    init

---
+++ src/components/networks/ResourceInformation.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="col-12">
+        {{value}}
+
+    </div>
+</template>
+<script lang="ts">
+import {Vue, Component , Prop , Watch} from 'vue-property-decorator'
+@Component({
+    components:{}
+})
+export default class AccountList extends Vue{
+    @Prop({default:() =>{return []}}) value:any;
+
+
+}
+</script>
