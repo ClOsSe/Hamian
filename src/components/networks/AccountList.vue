fd81ac419d88c12f2f5708ab3b2d040177d4aca1
mr_ceh <webmaster.webdeveloper@gmail.com>
2021-11-24 01:43:11 -0800

    add radio btn to select account

--- src/components/networks/AccountList.vue
+++ src/components/networks/AccountList.vue
@@ -2,19 +2,27 @@
     <div class="col-12">
         <div class="col-12 box" v-for="(account , index) in value" :key="index">
             <div class="acc-box" v-if="account.name">
+                <b-form-radio @click.native="setSelectedacc(account)"  name="some-radios" >
                 <i class="bx bx-user font-size-16 align-middle me-1"></i>  {{account.name}}({{account.authority}})
+                </b-form-radio>
             </div>
         </div>
     </div>
 </template>
 <script lang="ts">
-import {Vue, Component , Prop , Watch} from 'vue-property-decorator'
+import {Vue, Component , Prop , Watch} from 'vue-property-decorator';
+import StorageService from '@/localService/storageService'
 @Component({
     components:{}
 })
 export default class AccountList extends Vue{
     @Prop({default:() =>{return []}}) value:any;
-    
+    async setSelectedacc(account:any){
+       var data =  await StorageService.saveSelectedAccount(account);
+       if(data.message == true){
+           console.log('account selected')
+       }
+    }
 
 }
 </script>
